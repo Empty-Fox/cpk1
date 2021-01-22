8c86b6b1499afeea1f12ccd5e01e7f95fe165caf
Empty-Fox <ulyana250188@gmail.com>
2021-01-22 17:46:27 +0200

    work with counter and some corrects

--- js/scripPublish.js
+++ js/scripPublish.js
@@ -106,3 +106,78 @@ $('li.dropdown').hover(function () {
 }, function () {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
+
+// ............................................
+// Counter
+var counter = 0;
+var c = 4000000;
+c1 = 10;
+var i = setInterval(function () {
+    $(".counter h1").html("$ " + numberWithSpaces(c));
+    $(".counter hr").css("width", c1 / 2 + "%");
+    counter++;
+    c1++;
+    c++;
+    if (counter == 101) {
+        clearInterval(i);
+    }
+}, 10);
+
+var cz = 4000100;
+var timerCount = Math.floor(Math.random() * 10000) + 5000;
+function counterTimer() {
+    var counterz = 0;
+    if (cz <= 6000000) {
+        var iz = setInterval(function () {
+            var counterNumber = Math.floor(Math.random() * 100);
+            cz1 = cz + counterNumber;
+            var izz = setInterval(function () {
+                $(".counter h1").html("$ " + numberWithSpaces(cz));
+
+                counterz++;
+
+                cz++;
+                if (cz == cz1) {
+                    clearInterval(izz);
+                    cz == cz1;
+                }
+
+            }, 100);
+
+            clearInterval(iz);
+
+        }, 500);
+    }
+
+}
+
+setInterval(counterTimer, timerCount);
+
+
+
+function numberWithSpaces(с) {
+    return с.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+
+/////////////// page write text
+// function nextMsg(i) {
+//     if (messages.length == i) {
+//       i = 0;
+//     }
+//     $('#message').html(messages[i]).fadeIn(1000).delay(5000).fadeOut(1000, function () {
+//       nextMsg(i + 1);
+//     });
+//   };
+
+//   var messages = [
+//     "Meet your revenue potential",
+//     "Get higher results",
+//     "Make every interaction more meaningful"
+//   ];
+
+//   $('#message').hide();
+
+//   nextMsg(0);
+  //////////////////////////////////////////////
