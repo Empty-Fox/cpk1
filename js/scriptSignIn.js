99e2a61c186b2e9a6c6a6806baad1e95bec1cdea
Empty-Fox <ulyana250188@gmail.com>
2021-01-29 13:54:32 +0200

    fixed work for ios

--- js/scriptSignIn.js
+++ js/scriptSignIn.js
@@ -23,27 +23,28 @@ $('li.dropdown').hover(function () {
         let menu = $(selector);
         let button = menu.find('.burger-menu_button', '.burger-menu_lines');
         let links = menu.find('.burger-menu_link');
-        let overlay = menu.find('.burger-menu_overlay');
-
+        // let overlay = menu.find('.blackBackground');
+      
         button.on('click', (e) => {
-            e.preventDefault();
-            toggleMenu();
+          e.preventDefault();
+          toggleMenu();
         });
-
+      
         links.on('click', () => toggleMenu());
-        overlay.on('click', () => toggleMenu());
-
+        $('.blackBackground').on('click', () => toggleMenu());
+      
         function toggleMenu() {
-            menu.toggleClass('burger-menu_active');
-
-            if (menu.hasClass('burger-menu_active')) {
-                $('body').css('overlow', 'hidden');
-            } else {
-                $('body').css('overlow', 'visible');
-            }
+          menu.toggleClass('burger-menu_active');
+      
+          if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overlow', 'hidden');
+            $('.blackBackground').css('display', 'block');
+          } else {
+            $('body').css('overlow', 'visible');
+            $('.blackBackground').css('display', 'none');
+          }
         }
-    }
-
+      }
     burgerMenu('.burger-menu');
 
     function BurgerDropdownMenu() {
