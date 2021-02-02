8860f6356a2f629b7116bb256230cf16e09277c7
Empty-Fox <ulyana250188@gmail.com>
2021-02-02 09:59:23 +0200

    deleted button inde.html advertise

--- js/main.js
+++ js/main.js
@@ -102,6 +102,9 @@ window.onclick = function (event) {
 //Form contact us
 
 $(".t708__btn").click(function () {
+  $('.t-form__inputsbox').show();
+  $('.t708__popup-container').show();
+  $('.js-successbox').hide();
   $(this).parent().find(".t708__wrapper").fadeToggle();
   $(".t708__btn").toggleClass("t708__btn_active");
 });
@@ -126,4 +129,93 @@ var messages = [
 $('#message').hide();
 
 nextMsg(0);
+
+/////////////////
 //////////////////////////////////////////////
+
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validateName(name) {
+  return /^[A-Za-z ]+$/.test(name);
+}
+
+function validate(name, email, subject) {
+
+  const $wrongName = $("#error_name");
+  const $wrongEmail = $("#error_email");
+  const $wrongMessage = $("#error_message");
+
+  const $name_border = $("#name_contactUs_mainPage");
+  const $email_border = $("#email_contactUs_mainPage");
+  const $message_border = $("#message_contactUs_mainPage");
+
+  if (validateName(name) && validateEmail(email) && subject.length != 0) {
+    $('#name_contactUs_mainPage').val('');
+    $('#email_contactUs_mainPage').val('');
+    $('#message_contactUs_mainPage').val('');
+
+    $('.t-form__inputsbox').hide();
+    $('.js-successbox').show();
+    setTimeout(function () {
+      $(".t708__btn").click();
+      $('.t-form__inputsbox').hide();
+      $('.t708__popup-container').hide();
+    }, 2000);
+  } else {
+
+    if (!validateName(name)) {
+      $name_border.css("border", "1px solid red");
+      $wrongName.css("display", "block");
+      $("#name_contactUs_mainPage").focus(function () {
+        $('#name_contactUs_mainPage').val('');
+        $wrongName.css("display", "none");
+        $name_border.css("border", "none");
+      });
+
+    }
+    if (!validateEmail(email)) {
+      $email_border.css("border", "1px solid red");
+      $wrongEmail.css("display", "block");
+      $("#email_contactUs_mainPage").focus(function () {
+        $('#email_contactUs_mainPage').val('');
+        $wrongEmail.css("display", "none");
+        $email_border.css("border", "none");
+      });
+
+    }
+    if (subject.length == 0) {
+      $message_border.css("border", "1px solid red");
+      $wrongMessage.css("display", "block");
+      $("#message_contactUs_mainPage").focus(function () {
+        $('#message_contactUs_mainPage').val('');
+        $wrongMessage.css("display", "none");
+        $message_border.css("border", "none");
+      });
+
+    }
+  }
+return (name, email, subject);
+}
+
+/////////////////////////////SEND MAIN PAGE FORM CONTACT US///////////////////////////////////
+var mainPage_contactUs_click = document.getElementById("click_contactForm_main");
+
+mainPage_contactUs_click.addEventListener("click", function () {
+  var name = $("#name_contactUs_mainPage").val();
+  var email = $("#email_contactUs_mainPage").val();
+  var subject = $("#message_contactUs_mainPage").val();
+  if (validate(name, email, subject)) {
+
+    console.log('validate')
+  } else {
+    
+    console.log('not validate')
+  }
+
+  return false;
+});
+
--- js/scriptBecomeAdvert.js
+++ js/scriptBecomeAdvert.js
@@ -47,3 +47,91 @@ $(".cselect_sm").each(function () {
     });
 
 });
+
+
+/////////////////////////////////////////////////////////////////
+
+/////////////////
+//////////////////////////////////////////////
+
+
+  function validateName(name) {
+    return /^[A-Za-z ]+$/.test(name);
+  }
+  
+ function validateWebname (website) { 
+     return /^(https?:\/\/)?((?:[a-z0-9-]+\.)+([a-z]{2,}))(?:\/|$)/.test(website);
+    }
+
+ 
+   function validate(company_name, user_name, website, he_is, verticals, messenger) {    
+
+    const $company_name_border = $("#company_name");
+    const $user_name_border = $("#user_name");
+    const $website_border = $("#website");
+    //  const $company_name_border = $("#companyName");
+    // const $company_name_border = $("#companyName");
+    // const $company_name_border = $("#companyName");
+    
+
+    var company_name = $("#company_name").val();
+    var user_name = $("#user_name").val();
+    var website = $("#website").val();
+
+    if (validateName(company_name) && validateName(user_name) && validateWebname(website)) {
+        $('#company_name').val('');
+        $('#user_name').val('');
+        $('#website').val('');
+
+        console.log(website)    
+      } else {
+    
+        if (!validateName(company_name)) {
+          $company_name_border.css("border", "1px solid red");        
+          $("#company_name").focus(function () {
+            $('#company_name').val('');           
+            $company_name_border.css("border", "none");
+          });
+    
+        }     
+        if (!validateName(user_name)) {
+            $user_name_border.css("border", "1px solid red");        
+            $("#user_name").focus(function () {
+              $('#user_name').val('');           
+              $user_name_border.css("border", "none");
+            });
+      
+          }  
+        if (!validateWebname(website)) {
+            $website_border.css("border", "1px solid red");        
+            $("#website").focus(function () {
+              $('#website').val('');           
+              $website_border.css("border", "none");
+            });
+      
+          }  
+      }
+    return (company_name, user_name, website);
+}
+  
+  /////////////////////////////SEND MAIN PAGE FORM CONTACT US///////////////////////////////////
+  var sendFormBecameAdvertiser = document.getElementById("sendFormBecameAdvertiser");
+  
+  sendFormBecameAdvertiser.addEventListener("click", function () {
+    var company_name = $("#company_name").val();
+    // var user_name = $("#email_contactUs_mainPage").val();
+    // var website = $("#message_contactUs_mainPage").val();
+    // var he_is = $("#name_contactUs_mainPage").val();
+    // var verticals = $("#email_contactUs_mainPage").val();
+    // var messenger  = $("#message_contactUs_mainPage").val();
+
+    if (validate(company_name)) {
+  
+      console.log('validate '+company_name)
+    } else {
+      
+      console.log('not validate ' +company_name)
+    }
+  
+    return false;
+  });
